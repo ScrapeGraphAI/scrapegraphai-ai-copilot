293c6b3fe3acb177a40cab4ce88b86900d187755
Marco Vinciguerra <mvincig11@gmail.com>
2024-06-10 22:40:23 +0200

    add new question
    Co-Authored-By: Matteo Vedovati <68272450+vedovati-matteo@users.noreply.github.com>

--- implementation_creator.py
+++ implementation_creator.py
@@ -10,7 +10,7 @@ def extract_code(file_path):
 
     # Create a dictionary for the code snippet
     code_snippet = {
-        "prompt": f"how is implemented {file_name}",
+        "prompt": f"how is implemented {file_name} in Scrapegraphai?",
         "answer": code
     }
 
